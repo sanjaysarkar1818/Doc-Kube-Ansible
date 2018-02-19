3216925aed4baf7e9bc668f199cbab42294bf85e
sanjaysarkar1818 <k.sanjay1818@gmail.com>
2018-02-19 10:39:26 -0600

    Update kubeinstall.sh

--- redhat/kubeinstall.sh
+++ redhat/kubeinstall.sh
@@ -4,20 +4,9 @@ yum update
 swapoff -a
 
 # for docker
-sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
-
-# Setup Docker Repository
-
-sudo yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
-
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-
-
-sudo yum install docker-ce
+yum -y install yum-utils
+yum-config-manager --add-repo https://yum.dockerproject.org/repo/main/centos/7
+yum -y --nogpgcheck install docker-engine-1.12.6-1.el7.centos.x86_64
 
 cat << EOF > /etc/docker/daemon.json
 {
@@ -25,6 +14,8 @@ cat << EOF > /etc/docker/daemon.json
 }
 EOF
 
+systemctl enable docker
+service docker restart
 
 #Kubernetes Installation
 
